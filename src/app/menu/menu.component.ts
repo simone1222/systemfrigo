fabdd27f8da03b1f6297ddafe79b116616779a3f
simone1222 <114957126+simone1222@users.noreply.github.com>
2024-02-07 14:21:30 +0100

    ff

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,52 +1,43 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
-import * as emailjs from 'emailjs-com';
+import { Component, OnInit } from '@angular/core';
+import {
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { EmailService } from '../email.service';
-
-
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
-  constructor(private emailService:EmailService){}
- 
-  datiUtente!:UntypedFormGroup;
-   
+  constructor(private emailService: EmailService) {}
 
+  datiUtente!: UntypedFormGroup;
 
   ngOnInit(): void {
     this.datiUtente = new UntypedFormGroup({
-      email: new UntypedFormControl(null, [Validators.email, Validators.required]),
-      numero: new UntypedFormControl(null,Validators.required),
-      guasto: new UntypedFormControl(null,[Validators.maxLength(100),Validators.required]),
-    }); 
+      email: new UntypedFormControl(null, [
+        Validators.email,
+        Validators.required,
+      ]),
+      numero: new UntypedFormControl(null, Validators.required),
+      guasto: new UntypedFormControl(null, [
+        Validators.maxLength(100),
+        Validators.required,
+      ]),
+    });
   }
 
-   // Funzione chiamata dopo che la vista è stata inizializzata
-  
-  /*  inviaEmail(form:UntypedFormGroup) {
-     if(form.valid){
-      console.log("inviata",form)
-    }
-    
-    
-   }
- */
-//Da vedere
-   inviaEmail(datiUtente: UntypedFormGroup) {
+  inviaEmail(datiUtente: UntypedFormGroup) {
     if (datiUtente.valid) {
       const email = String(datiUtente.get('email')?.value);
       const numero = String(datiUtente.get('numero')?.value);
       const guasto = String(datiUtente.get('guasto')?.value);
-      this.emailService.invioEmail(email,numero,guasto).subscribe((c)=>{
-        console.log(email,numero,guasto)
-
+      this.emailService.invioEmail(email, numero, guasto).subscribe((c) => {
+        
       });
     }
-    }
-
- 
+  }
 }
